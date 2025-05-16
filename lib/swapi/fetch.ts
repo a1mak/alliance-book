0ce217c4dcf49565d923be603b684056123aa895
Oleksandr Makarenko <zloynightmare@gmail.com>
2025-05-16 17:30:14 +0200

    Add fetch all resource pages

--- lib/swapi/fetch.ts
+++ lib/swapi/fetch.ts
@@ -18,7 +18,7 @@ export const fetchSwapi = async <T extends keyof SchemaMapByUrl>(
   { page, search }: FetchSwapiConfig,
 ) =>
   unstable_cache(
-    async (resource) => {
+    async (resource: T) => {
       const searchParams = new URLSearchParams([["page", page.toString()]])
 
       if (search) {
@@ -55,5 +55,48 @@ export const fetchSwapi = async <T extends keyof SchemaMapByUrl>(
     {
       revalidate: revalidationTime,
       tags: [resource, page.toString(), search ?? ""],
+    },
+  )(resource)
+
+export const fetchSwapiAllPages = async <T extends keyof SchemaMapByUrl>(
+  resource: T,
+) =>
+  unstable_cache(
+    async (resource: T) => {
+      const response = await fetchSwapi(resource, {
+        page: 1,
+      })
+
+      if (response.status === "failure") {
+        return {
+          error: response.error,
+          status: response.status,
+        }
+      }
+
+      const { count, results } = response.data
+      const totalPages = Math.ceil(count / results.length)
+
+      const peopleRestResponses = await Promise.all(
+        Array.from({ length: totalPages - 1 }, (_, i) =>
+          fetchSwapi(resource, {
+            page: i + 2,
+          }),
+        ),
+      )
+
+      peopleRestResponses.forEach((response) => {
+        if (response.status === "failure") {
+          return
+        }
+        results.push(...response.data.results)
+      })
+
+      return { data: { results, count }, status: "success" as const }
+    },
+    [resource],
+    {
+      revalidate: revalidationTime,
+      tags: [resource],
     },
   )(resource)
